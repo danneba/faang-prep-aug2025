1d38c19d1f123397ec07613656b2fcd1e2e7d34f
Daniel Wassie <daniel.kassahun@populationexplorer.com>
2025-08-19 14:28:09 +0300

    Day 1: Arrays & Strings – Two Sum optimized (hashmap approach)

--- week1/day1_arrays_strings/two_sum.py
+++ week1/day1_arrays_strings/two_sum.py
@@ -1,14 +1,14 @@
 from typing import List 
 
 class Solution:
-    def twoSum(self, nums:List[int], target: int)-> List[int]:
-        length = len(nums)
-        for i in range(length):
-            for j in range(length):
-                if i != j and nums[i] + nums[j] == target:
-                    return [i , j]
-                else:
-                    continue
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sum_pair = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in sum_pair:
+                return [sum_pair[diff], i]
+            else:
+                sum_pair[nums[i]] = i
 
 if __name__ == "__main__":
     # Local test cases
